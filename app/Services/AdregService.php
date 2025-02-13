4474cb283b7c688274a1c71784e29aad9833a19a
Hussain <hussain.yusuf95@gmail.com>
2025-02-13 11:25:30 +0300

    First commit

---
+++ app/Services/AdregService.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Services;
+
+/**
+ * Class AdregService.
+ */
+class AdregService
+{
+    public function colleges()
+    {
+        //
+    }
+
+    public function departments()
+    {
+        //
+    }
+
+}
---
+++ config/sync.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    'adreg' => [
+        'url' => env('ADREG_API_URL', 'https://localhost'),
+    ],
+
+    'moodle' => [
+        'root' => env('MOODLE_ROOT', '/var/www/html/'),
+    ],
+
+
+];
