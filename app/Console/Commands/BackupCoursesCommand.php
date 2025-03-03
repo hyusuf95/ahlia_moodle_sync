a7ff370cb19569c87be14155fad2bd5f1428fdc8
Hussain <hussain.yusuf95@gmail.com>
2025-03-03 15:02:31 +0300

    Test command

---
+++ app/Console/Commands/BackupCoursesCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\MoodleCourse;
+use App\Services\AdregService;
+use Illuminate\Console\Command;
+
+class BackupCoursesCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'backup:courses {--semester=}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //if semester not provided, ask for it
+        $semester_id =  (int) ($this->option('semester'));
+
+        $adreg = new AdregService();
+        $courses = $adreg->sections($semester_id);
+
+        $moodle_path = config('sync.moodle.source_root');
+        $backup_folder = config('sync.moodle.courses.backup_folder');
+
+        foreach ($courses as $course) {
+            $idnumber = $course->section_id;
+            $moodle_id = MoodleCourse::find2($idnumber)->id;
+
+            $command = "php $moodle_path/admin/cli/backup.php --courseid=$moodle_id --destination=$moodle_path/$backup_folder";
+
+            dd($command);
+        }
+    }
+}
